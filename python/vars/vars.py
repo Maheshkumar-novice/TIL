b33f8d9d7f77a0b8e708f17343b8716d5d59d832
Maheshkumar P <67100964+Maheshkumar-novice@users.noreply.github.com>
2023-04-21 13:31:04 +0530

    Create vars.py

---
+++ python/vars/vars.py
@@ -0,0 +1,22 @@
+pprint.pprint(vars())
+
+'''
+Lists names for the provided object. Default current module. Similar to dir() but
+shows names with values. Looks like little limited to dir() but not sure how. Need to learn.
+
+output:
+
+{'Person': <class '__main__.Person'>,
+ '__annotations__': {},
+ '__builtins__': <module 'builtins' (built-in)>,
+ '__doc__': None,
+ '__loader__': <_frozen_importlib_external.SourceFileLoader object at 0x102857c10>,
+ '__name__': '__main__',
+ '__package__': None,
+ '__spec__': None,
+ '__warningregistry__': {'version': 0},
+ 'os': <module 'os' from '/opt/homebrew/Cellar/python@3.10/3.10.9/Frameworks/Python.framework/Versions/3.10/lib/python3.10/os.py'>,
+ 'p': <__main__.Person object at 0x1031bf100>,
+ 'pprint': <module 'pprint' from '/opt/homebrew/Cellar/python@3.10/3.10.9/Frameworks/Python.framework/Versions/3.10/lib/python3.10/pprint.py'>,
+ 'sys': <module 'sys' (built-in)>}
+'''
