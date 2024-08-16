06a35a26d24f97fdd9b3f42a3a82ed06204fd8e0
Maheshkumar P <67100964+Maheshkumar-novice@users.noreply.github.com>
2024-08-16 15:52:41 +0530

    Create file_stream_redirect.cpp

---
+++ cpp/file_stream_redirect.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    if (freopen("input.txt", "r", stdin) == nullptr)
+    {
+        cerr << "Failed to open input file." << std::endl;
+        return 1;
+    }
+
+    if (freopen("output.txt", "w", stdout) == nullptr)
+    {
+        cerr << "Failed to open output file." << std::endl;
+        return 1;
+    }
+
+    string a;
+    cin >> a;
+    cout << a << "\n";
+    cin >> a;
+    cout << a << "\n";
+}
